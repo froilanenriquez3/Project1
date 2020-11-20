97cc4f2c5a78785ecd6484cc4948a88a4f80fdc8
Froilan-examen <fenriquez31@cepnet.net>
2020-11-20 10:32:29 +0100

    Add database sql file

---
+++ database/restore.sql
@@ -0,0 +1,133 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema restore
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `restore` ;
+
+-- -----------------------------------------------------
+-- Schema restore
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `restore` DEFAULT CHARACTER SET utf8 ;
+USE `restore` ;
+
+-- -----------------------------------------------------
+-- Table `restore`.`user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `restore`.`user` ;
+
+CREATE TABLE IF NOT EXISTS `restore`.`user` (
+  `userid` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(16) NOT NULL,
+  `password` VARCHAR(16) NOT NULL,
+  `points` INT NULL,
+  `isAdmin` TINYINT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`userid`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `restore`.`game`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `restore`.`game` ;
+
+CREATE TABLE IF NOT EXISTS `restore`.`game` (
+  `idgame` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `pointLimit` INT NULL,
+  PRIMARY KEY (`idgame`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `restore`.`user_plays_game`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `restore`.`user_plays_game` ;
+
+CREATE TABLE IF NOT EXISTS `restore`.`user_plays_game` (
+  `users_userid` INT UNSIGNED NOT NULL,
+  `games_idgame` INT NOT NULL,
+  `pointSave` TINYINT NULL,
+  `highScore` INT NULL,
+  PRIMARY KEY (`users_userid`, `games_idgame`),
+  INDEX `fk_users_has_games_games1_idx` (`games_idgame` ASC) ,
+  INDEX `fk_users_has_games_users_idx` (`users_userid` ASC) ,
+  CONSTRAINT `fk_users_has_games_users`
+    FOREIGN KEY (`users_userid`)
+    REFERENCES `restore`.`user` (`userid`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_users_has_games_games1`
+    FOREIGN KEY (`games_idgame`)
+    REFERENCES `restore`.`game` (`idgame`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `restore`.`store`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `restore`.`store` ;
+
+CREATE TABLE IF NOT EXISTS `restore`.`store` (
+  `idstore` INT NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `desc` VARCHAR(45) NULL,
+  PRIMARY KEY (`idstore`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `restore`.`promotion`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `restore`.`promotion` ;
+
+CREATE TABLE IF NOT EXISTS `restore`.`promotion` (
+  `idpromotion` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `desc` VARCHAR(45) NULL,
+  `pointCost` INT NULL,
+  `store_idstore` INT NOT NULL,
+  PRIMARY KEY (`idpromotion`),
+  INDEX `fk_promotion_store1_idx` (`store_idstore` ASC) ,
+  CONSTRAINT `fk_promotion_store1`
+    FOREIGN KEY (`store_idstore`)
+    REFERENCES `restore`.`store` (`idstore`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `restore`.`user_has_promotion`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `restore`.`user_has_promotion` ;
+
+CREATE TABLE IF NOT EXISTS `restore`.`user_has_promotion` (
+  `user_userid` INT UNSIGNED NOT NULL,
+  `promotion_idpromotion` INT NOT NULL,
+  PRIMARY KEY (`user_userid`, `promotion_idpromotion`),
+  INDEX `fk_user_has_promotion_promotion1_idx` (`promotion_idpromotion` ASC) ,
+  INDEX `fk_user_has_promotion_user1_idx` (`user_userid` ASC) ,
+  CONSTRAINT `fk_user_has_promotion_user1`
+    FOREIGN KEY (`user_userid`)
+    REFERENCES `restore`.`user` (`userid`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_has_promotion_promotion1`
+    FOREIGN KEY (`promotion_idpromotion`)
+    REFERENCES `restore`.`promotion` (`idpromotion`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
