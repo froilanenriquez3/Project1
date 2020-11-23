036b96819af3984e11ffa21532dfc83db8126bf7
Froilan-examen <fenriquez31@cepnet.net>
2020-11-23 13:31:27 +0100

    Modify bd.php

--- php_libraries/bd.php
+++ php_libraries/bd.php
@@ -1,5 +1,7 @@
 <?php
 
+insertUser("Froilan", "1235", 0, 0, "frilan@whatev.com");
+
 
 function openDB(){
     $servername = "localhost";
@@ -91,7 +93,41 @@ function selectUserGameInfo($user_id) {
 
 }
 
+function insertUser($username, $password, $points, $isAdmin, $email){
+    $games = [1,2,3,4];
 
+    $connection = openDB();
+    $connection->beginTransaction();
+
+    $mySQLsentence = "INSERT INTO user VALUE(null, :username, :usepassword, :points, :isAdmin, :email)";
+
+    $mySQLsentence= $connection ->prepare($mySQLsentence);
+    $mySQLsentence->bindParam(":username",$username);
+    $mySQLsentence->bindParam(":usepassword",$password);  
+    $mySQLsentence->bindParam(":points",$points); 
+    $mySQLsentence->bindParam(":isAdmin",$isAdmin); 
+    $mySQLsentence->bindParam(":email",$email); 
+
+    $mySQLsentence->execute();
+    $lastId = $connection->lastInsertId();
+
+    foreach ($games as $game){
+
+        $mySQLsentence2 = "INSERT INTO user_plays_game VALUES(:userid, :gameid, 0, 0)";
+
+        $mySQLsentence2 = $connection ->prepare($mySQLsentence2);
+
+        $mySQLsentence2->bindParam(":userid",$lastId);
+        $mySQLsentence2->bindParam(":gameid",$game);
+        $mySQLsentence2->execute();
+    }
+    
+    $connection->commit();
+    $connection = closeDB();
+
+ 
+
+}
 
 
 ?>
