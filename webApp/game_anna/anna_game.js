ea200928a51c382518da2790e898b51fdf481f98
anuskam <anuskam92@gmail.com>
2020-11-30 13:12:12 +0100

    borro comentarios del juego

--- webApp/game_anna/anna_game.js
+++ webApp/game_anna/anna_game.js
@@ -8,13 +8,12 @@ function start(){
   let bottom = 0;
   let gravity = 0.9;
   let isJumping = false;
-  let isGoingLeft = false; //a menos que se clique la flecha no se moverá a la izquierda
   let left = 0; //punto de partida para calcular el movimiento hacia la izquierda
+  let velocidad = 15;
+  let isGoingLeft = false; //a menos que se clique la flecha no se moverá a la izquierda
   let isGoingRight = false;
   let timerRight;
   let timerLeft;
-  let velocidad = 15;
-  let coins = [];
 
   
   // function fall(){
@@ -207,102 +206,3 @@ function start(){
 //   character.x +=5;
 //   square.style.left = character.x+"px";
 // }
-
-
-
-// document.addEventListener('click', () => {
-//     const character = document.querySelector('.character')
-//     let isJumping = false
-//     let isGoingRight = false
-//     let isGoingLeft = false
-//     let bottom = 0
-//     let gravity = 0.9
-//     let left = 0
-//     //let leftTimerId
-//     let rightTimerId
-
-//     function jump() {
-//         if (isJumping) return
-//         //character.classList.remove('character-sliding')
-//         //character.classList.add('character')
-//         let upTimerId = setInterval(function () {
-//           //jump down
-//           if (bottom > 250) {
-//             clearInterval(upTimerId)
-//             let downTimerId = setInterval(function () {
-//               if (bottom < 0 ) {
-//                 clearInterval(downTimerId)
-//                 isJumping = false
-//               }
-//               bottom -= 4
-//               bottom = bottom * gravity
-//               character.style.bottom = bottom + 'px'
-//             },20)
-//           }
-//           //jump up
-//           isJumping = true
-//           bottom +=30
-//           bottom = bottom * gravity
-//           character.style.bottom = bottom + 'px'
-//         },20)
-//       }
-
-//       // function slideLeft() {
-//       //   // prince.classList.remove('character')
-//       //   // prince.classList.add('character-sliding')
-//       //   if (isGoingRight) {
-//       //       clearInterval(rightTimerId)
-//       //       isGoingRight = false
-//       //   }
-//       //   isGoingLeft = true
-//       //   leftTimerId = setInterval(function () {
-//       //       console.log('going left')
-//       //       left -=5
-//       //       character.style.left = left + 'px'
-//       //   },20)
-//       // }
-
-//       // function slideRight() {
-//       //   // prince.classList.remove('character')
-//       //   // prince.classList.add('character-sliding')
-//       //   if (isGoingLeft) {
-//       //       //clearInterval(leftTimerId)
-//       //       isGoingLeft = false
-//       //   }
-//       //   isGoingRight = true
-//       //   rightTimerId = setInterval(function () {
-//       //       console.log('going right')
-//       //       left +=5
-//       //       character.style.left = left + 'px'
-//       //   },20)
-//       // }
-
-//     function moveRight(){
-//       isGoingRight = true;
-//       rightTimerId = setInterval(function(){
-//         console.log('going right');
-//         left += 5;
-//         character.style.left = left + 'px'
-//       },20)
-//     }
-
-
-//     //assign functions to keycodes
-//     function control(e) {
-//         if(e.keyCode === 39) {
-//           moveRight()
-//             // slideRight() //if we press the right arrow on our keyboard
-//         } else if (e.keyCode === 32) {
-//             jump() // if we press the up arrow
-//         } else if (e.keyCode === 37) {
-//             slideLeft() // if we press left
-//         } 
-//     }
-//     document.addEventListener('keyup', control)
-
-
-//assign functions to keycodes
-
-
-//document.addEventListener('keyup', control)
-// })
