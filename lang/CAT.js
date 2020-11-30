8dc741de95f39c86871e23e73e68e3aebe33471d
Mireia <misala91@gmail.com>
2020-11-30 11:28:14 +0100

    add text divulgation

--- lang/CAT.js
+++ lang/CAT.js
@@ -1,1 +1,4 @@
-
+let cat={
+    divulgation1: "El 96% dels petits negocis considera que la crisi provocada per la pandèmia del coronavirus afectarà de forma negativa o molt negativa a la seva activitat. Les restriccions d'aforament i el tancament temporal d'alguns d'aquests negocis ha fet que molts d'ells es trobin en una situació econòmica difícil. Per ajudar en aquests moments crítics neix 'Recomerçem'." ,
+    divulgation2: "'Recomerçem' té com a objectiu potenciar les compres en comerços locals a Barcelona. Per aquest motiu hem creat quatre jocs, mitjançant els quals podem acumular punts per accedir a promocions i descomptes a diferents tendes. T'apuntes? Segueix cap avall per conèixer a la Teresa i descobrir la seva història!"
+}
