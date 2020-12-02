f7dfdd6c316332b327044226b14081d6751f987d
Froilan-examen <fenriquez31@cepnet.net>
2020-12-02 12:13:24 +0100

    Implement logout

--- php_partials/buttons.php
+++ php_partials/buttons.php
@@ -1,5 +1,13 @@
 <div id="buttons">
-    <a role="button" class="btn btn-sm" href="/project1/php_views/login.php">
+    <a role="button" class="btn btn-sm" href=" 
+        <?php if(isset($_SESSION['user']))
+        {   
+            $_SESSION['logmeout'] = true;
+            echo "/project1/php_controllers/login_controller.php";}
+        else{ 
+            echo "/project1/php_views/login.php";
+        } ?>   ">
+
         <?php if(isset($_SESSION['user'])){echo "Log out";} else{echo "Log in";}?>
     </a>
     <?php if(!isset($_SESSION['user'])){?>
