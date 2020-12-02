27b84023627cc287e7b09722497c1ff400a8b150
Froilan-examen <fenriquez31@cepnet.net>
2020-12-02 14:32:21 +0100

    Fix dissapearing items bug

--- webApp/game_froilan/scripts/eventListeners.js
+++ webApp/game_froilan/scripts/eventListeners.js
@@ -43,6 +43,7 @@ document.addEventListener("dragleave", function (event) {
 }, false);
  */
 
+//Event lisetener for dragging items
 document.addEventListener("drop", function (event) {
   // prevent default action (open as link for some elements)
   event.preventDefault();
@@ -61,75 +62,80 @@ document.addEventListener("drop", function (event) {
 
 //Event listener for dragging items over occupied squares
 document.addEventListener("drop", function (event) {
- 
-  event.preventDefault();
 
+  event.preventDefault();
 
   if ((event.target.classList.contains("dragHat") && dragged.classList.contains("dragHat"))
     || (event.target.classList.contains("dragShirt") && dragged.classList.contains("dragShirt"))
     || (event.target.classList.contains("dragPants") && dragged.classList.contains("dragPants"))
     || (event.target.classList.contains("dragShoes") && dragged.classList.contains("dragShoes"))
   ) {
-  
-    let destination = locateDestination(event.target);
-    
-    if(event.target.parentNode.parentNode.classList.contains("center")){
-      dragged.parentNode.removeChild(dragged);
-      event.target.parentNode.appendChild(dragged);
-      event.target.parentNode.removeChild(event.target.parentNode.childNodes[0]);
 
-      document.querySelector('#'+destination+"").appendChild(event.target);
+    let destination = locateDestination(event.target);
+    if (event.target.parentNode.parentNode.classList.contains("center")) {
+
+
+      if (!dragged.parentNode.parentNode.classList.contains("center")) {
+        console.log(dragged.parentNode);
+        dragged.parentNode.removeChild(dragged);
+        console.log("success removal1");
+      }
+
+      event.target.parentNode.appendChild(dragged);
+      console.log("success appending1");
+      event.target.parentNode.removeChild(event.target.parentNode.childNodes[0]);
+      console.log("success removal2");
+      document.querySelector('#' + destination + "").appendChild(event.target);
+      console.log("success appending2");
+
     }
-    
-    
 
   }
-
 
   event.target.style.background = "";
 }, false);
 
-
+//Find original location of clothing that is being dragged over
 function locateDestination(replaced) {
   let zone = replaced.classList;
   let zoneStr;
   let location;
-  
+
   //Find the zone
-  if(zone.contains("dragHat")){
+  if (zone.contains("dragHat")) {
     zoneStr = "hatzone";
   }
-  if(zone.contains("dragShirt")){
+  if (zone.contains("dragShirt")) {
     zoneStr = "shirtzone";
   }
-  if(zone.contains("dragPants")){
+  if (zone.contains("dragPants")) {
     zoneStr = "pantszone";
   }
-  if(zone.contains("dragShoes")){
+  if (zone.contains("dragShoes")) {
     zoneStr = "shoeszone";
   }
- 
+
 
   //Find the number
-  if(replaced.id.includes("1")){
+  if (replaced.id.includes("1")) {
     location = zoneStr + "1";
   }
-  if(replaced.id.includes("2")){
+  if (replaced.id.includes("2")) {
     location = zoneStr + "2";
   }
-  if(replaced.id.includes("3")){
+  if (replaced.id.includes("3")) {
     location = zoneStr + "3";
   }
-  if(replaced.id.includes("4")){
+  if (replaced.id.includes("4")) {
     location = zoneStr + "4";
   }
-  if(replaced.id.includes("5")){
+  if (replaced.id.includes("5")) {
     location = zoneStr + "5";
   }
-  if(replaced.id.includes("6")){
+  if (replaced.id.includes("6")) {
     location = zoneStr + "6";
   }
 
-  console.log(location);
+
   return location;
 }
