abf6c662d9deeb46c7f1a2384d10c32886693e68
Mireia <misala91@gmail.com>
2020-12-02 15:47:31 +0100

    modify games page

--- php_views/games.php
+++ php_views/games.php
@@ -4,19 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Recomençem-Jocs</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-    crossorigin="anonymous"></script>
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-    crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-    crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" 
+    integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="/project1/style/style-navbar.css">
     <link rel="stylesheet" href="/project1/style/buttons.css">
+    <link rel="stylesheet" href="/project1/style/games.css">
 </head>
 <body>
 <?php   
@@ -24,8 +16,54 @@
     include '../php_partials/buttons.php';
 ?>  
 
+
 <div class="container">
-    
+<div class="row">
+<div class="col-lg-6 pt-2 pb-2">
+  <a href="../webApp/game_anna/index.html">
+<div class="card mx-auto">
+  <div class="card-body">
+    <h5 class="card-title">Juego 1</h5>
+  </div>
+  <img src="../media/img/game4screenshot.png" class="card-img-bottom h-80" alt="game1">
+</div>
+  </a>
+</div>
+<div class="col-lg-6 pt-2 pb-2">
+  <a href="../webApp/game_froilan/index.html">
+<div class="card mx-auto">
+  <div class="card-body">
+    <h5 class="card-title">Juego 2</h5>
+   
+  </div>
+  <img src="../media/img/game4screenshot.png" class="card-img-bottom h-80" alt="game2">
+</div>
+  </a>
+</div>
+</div>
+<div class="row">
+  <div class="col-lg-6 pt-2 pb-2">
+    <a href="../webApp/game_alex/index.html">
+<div class="card mx-auto">
+  <div class="card-body">
+    <h5 class="card-title">Juego 3</h5>
+  </div>
+  <img src="../media/img/game4screenshot.png" class="card-img-bottom h-80" alt="game3">
+</div>
+    </a>
+</div>
+<div class="col-lg-6 pt-2 pb-2">
+  <a href="../webApp/game_mireia/index.html">
+<div class="card mx-auto">
+  <div class="card-body">
+    <h5 class="card-title">Juego 4</h5>
+  </div>
+  <img src="../media/img/game4screenshot.png" class="card-img-bottom h-80" alt="game4">
+</div>
+  </a>
+</div>
+</div>
+
 </div>
 <script src="../js/navbar.js"></script>
 </body>
