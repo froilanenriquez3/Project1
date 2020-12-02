bd2cac3d8916a44e5c9e49b47fcf5e0f60213986
Froilan-examen <fenriquez31@cepnet.net>
2020-12-02 12:02:12 +0100

    Hide registration button when user is logged in

--- php_partials/buttons.php
+++ php_partials/buttons.php
@@ -2,5 +2,7 @@
     <a role="button" class="btn btn-sm" href="/project1/php_views/login.php">
         <?php if(isset($_SESSION['user'])){echo "Log out";} else{echo "Log in";}?>
     </a>
-    <a role="button" class="btn btn-sm" href="/project1/php_views/signup.php">Registrarse</a>
+    <?php if(!isset($_SESSION['user'])){?>
+        <a role="button" class="btn btn-sm" href="/project1/php_views/signup.php">Registrarse</a>
+    <?php }?>
 </div>
