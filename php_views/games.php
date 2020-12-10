6a613884513cf3bacfce83270155c92e2c54f92e
Froilan-examen <fenriquez31@cepnet.net>
2020-12-10 11:45:01 +0100

    Implement locking of games

--- php_views/games.php
+++ php_views/games.php
@@ -1,3 +1,12 @@
+<?php 
+  require_once '../php_libraries/bd.php';
+  $game_info1 = selectUserGameInfo($_SESSION['user']['userid'],1);
+  $game_info2 = selectUserGameInfo($_SESSION['user']['userid'],2);
+  $game_info3 = selectUserGameInfo($_SESSION['user']['userid'],3);
+  $game_info4 = selectUserGameInfo($_SESSION['user']['userid'],4);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -29,52 +38,58 @@
 
 <div class="container">
 <div class="row">
-<div class="col-lg-6 pt-2 pb-2">
-  <a href="../webApp/game_anna/index.html">
-<div class="card mx-auto">
-  <div class="card-body">
-    <h5 class="card-title">Juego 1</h5>
-  </div>
-  <img src="../media/img/game4screenshot.png" class="card-img-bottom h-80" alt="game1">
-</div>
-  </a>
-</div>
-<div class="col-lg-6 pt-2 pb-2">
-  <a href="../webApp/game_froilan/index.php">
-<div class="card mx-auto">
-  <div class="card-body">
-    <h5 class="card-title">Juego 2</h5>
-   
-  </div>
-  <img src="../media/img/game2screenshot.png" class="card-img-bottom h-80" alt="game2">
-</div>
-  </a>
-</div>
-</div>
-<div class="row">
   <div class="col-lg-6 pt-2 pb-2">
-    <a href="../webApp/game_alex/index.html">
-<div class="card mx-auto">
-  <div class="card-body">
-    <h5 class="card-title">Juego 3</h5>
+    <a href="../webApp/game_anna/index.html">
+  <div class="card mx-auto">
+    <div class="card-body">
+      <h5 class="card-title">Juego 1</h5>
+    </div>
+    <img src="../media/img/game4screenshot.png" class="card-img-bottom h-80" alt="game1" id="game_mireia">
   </div>
-  <img src="../media/img/game4screenshot.png" class="card-img-bottom h-80" alt="game3">
-</div>
     </a>
-</div>
-<div class="col-lg-6 pt-2 pb-2">
-  <a href="../webApp/game_mireia/index.php">
-<div class="card mx-auto">
-  <div class="card-body">
-    <h5 class="card-title">Juego 4</h5>
   </div>
-  <img src="../media/img/game4screenshot.png" class="card-img-bottom h-80" alt="game4">
+
+  <div class="col-lg-6 pt-2 pb-2  <?php if($game_info1['pointSave'] == 0){ echo "locked";}?> " id="game_froilan">
+    <a href="../webApp/game_froilan/index.php">
+    <div class="card mx-auto">
+      <div class="card-body">
+        <h5 class="card-title">Juego 2</h5>
+    
+      </div>
+      <img src="../media/img/game2screenshot.png" class="card-img-bottom h-80" alt="game2">
+    </div>
+    </a>
+  </div>
 </div>
-  </a>
-</div>
+
+<div class="row">
+
+    <div class="col-lg-6 pt-2 pb-2 <?php if($game_info2['pointSave'] == 0){ echo "locked";}?>" id="game_alex">
+      <a href="../webApp/game_alex/index.html">
+  <div class="card mx-auto">
+    <div class="card-body">
+      <h5 class="card-title">Juego 3</h5>
+    </div>
+    <img src="../media/img/game4screenshot.png" class="card-img-bottom h-80" alt="game3">
+  </div>
+      </a>
+  </div>
+
+  <div class="col-lg-6 pt-2 pb-2 <?php if($game_info3['pointSave'] == 0){ echo "locked";}?>" id="game_mireia">
+    <a href="../webApp/game_mireia/index.php">
+    <div class="card mx-auto">
+    <div class="card-body">
+      <h5 class="card-title">Juego 4</h5>
+      </div>
+      <img src="../media/img/game4screenshot.png" class="card-img-bottom h-80" alt="game4">
+    </div>
+    </a>
+  </div>
+
 </div>
 
 </div>
 <script src="../js/navbar.js"></script>
 </body>
 </html>
+ -->
