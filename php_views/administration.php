ca1b7210aef266548b8b414379bfd255f0a7724d
anuskam <anuskam92@gmail.com>
2020-12-13 23:50:50 +0100

    administration hecha

--- php_views/administration.php
+++ php_views/administration.php
@@ -25,12 +25,17 @@ if ($_SESSION['user']['isAdmin'] == 0) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">        <link rel="stylesheet" href="../style/style-navbar.css">
         <link rel="stylesheet" href="../style/buttons.css">
-        <link rel="stylesheet" href="../style/administration2.css">
+        <link rel="stylesheet" href="../style/administration.css">
         <!-- <script src="../js/administration2.js"></script> -->
 
         <!-- FONT MONTSERRAT -->
         <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">    
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">   
+        
+        <!-- FONT OWSWALD BOLD -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@600&display=swap" rel="stylesheet">
+
 
         <title>Restore</title>
     </head>
@@ -49,7 +54,9 @@ if ($_SESSION['user']['isAdmin'] == 0) {
                 </div>
 
                 <div class="card-body">
-                <p>Aquí puedes mantener los administradores, usuarios, promociones y puntos</p>
+                    <div class="info">
+                        <p>Aquí puedes mantener los administradores, usuarios, promociones y puntos</p>
+                    </div>
 
                     <div class="accordion" id="admin-accordion">
                         <!-- PRIMER ITEM -->
