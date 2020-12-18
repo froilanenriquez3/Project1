d91988643f7b385e605464efbcda545dffec31a9
Froilan-examen <fenriquez31@cepnet.net>
2020-12-18 09:17:20 +0100

    Fix fonts

--- php_views/gamestats.php
+++ php_views/gamestats.php
@@ -14,6 +14,10 @@ $high4 = selectHighScores(4);
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="../style/style-navbar.css">
     <link rel="stylesheet" href="../style/buttons.css">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+    
     <title>Estadísticas</title>
     <link rel="shortcut icon" type="image/png" href="/project1/media/img/logo.png"/>
 </head>
